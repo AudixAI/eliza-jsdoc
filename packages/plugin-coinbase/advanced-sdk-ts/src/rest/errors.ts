6b191a8dfdc3d0b43533d89d99318bae5388e933
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2025-01-10 16:03:43 -0500

    docs: Add JSDoc documentation to errors.ts

--- packages/plugin-coinbase/advanced-sdk-ts/src/rest/errors.ts
+++ packages/plugin-coinbase/advanced-sdk-ts/src/rest/errors.ts
@@ -1,8 +1,26 @@
 import { Response } from 'node-fetch';
 
+/**
+ * Represents an error specific to the Coinbase API.
+ *
+ * @class
+ * @extends Error
+ * @param {string} message - The error message.
+ * @param {number} statusCode - The status code of the response.
+ * @param {Response} response - The full response object.
+ */
+       
 class CoinbaseError extends Error {
     statusCode: number;
     response: Response;
+
+/**
+ * Constructor for creating a new CoinbaseError instance.
+ *
+ * @param {string} message - The error message.
+ * @param {number} statusCode - The status code associated with the error.
+ * @param {Response} response - The response object associated with the error.
+ */
 
     constructor(message: string, statusCode: number, response: Response) {
         super(message);
@@ -12,6 +30,14 @@ class CoinbaseError extends Error {
     }
 }
 
+/**
+ * Handles exceptions for Coinbase API requests.
+ * Throws a custom CoinbaseError if the response status indicates an error from the Coinbase API.
+ *
+ * @param {Response} response - The response object from the API request.
+ * @param {string} responseText - The response text from the API request.
+ * @param {string} reason - The reason for the error.
+ */
 export function handleException(
     response: Response,
     responseText: string,
