c492bc580093b525e7978068bc7c475f06eb4a84
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2025-01-10 16:01:21 -0500

    docs: Add JSDoc documentation to types.ts

--- packages/client-lens/src/types.ts
+++ packages/client-lens/src/types.ts
@@ -1,3 +1,14 @@
+/**
+ * Represents a user profile.
+ * @typedef {Object} Profile
+ * @property {string} id - The unique identifier for the profile.
+ * @property {string} profileId - The profile identifier.
+ * @property {string|null} [name] - The name of the user.
+ * @property {string} [handle] - The user's handle.
+ * @property {string} [pfp] - The profile picture of the user.
+ * @property {string|null} [bio] - The biography of the user.
+ * @property {string} [url] - The URL associated with the user's profile.
+ */
 export type Profile = {
     id: string;
     profileId: string;
@@ -8,6 +19,12 @@ export type Profile = {
     url?: string;
 };
 
+/**
+ * Represents the result of a broadcast transaction.
+ * @typedef {Object} BroadcastResult
+ * @property {string} [id] - The ID of the broadcast result.
+ * @property {string} [txId] - The transaction ID of the broadcast result.
+ */
 export type BroadcastResult = {
     id?: string;
     txId?: string;
