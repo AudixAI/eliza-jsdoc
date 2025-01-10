74084385203d9dc5fd95243878a1b80c200666c0
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2025-01-10 16:03:41 -0500

    docs: Add JSDoc documentation to accounts.ts

--- packages/plugin-coinbase/advanced-sdk-ts/src/rest/accounts.ts
+++ packages/plugin-coinbase/advanced-sdk-ts/src/rest/accounts.ts
@@ -10,6 +10,11 @@ import { method } from './types/request-types';
 
 // [GET] Get Account
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_getaccount
+/**
+ * Retrieves information about a specific account from the API.
+ * @param {GetAccountRequest} params - The parameters for the request, including the accountUuid to retrieve.
+ * @returns {Promise<GetAccountResponse>} A promise that resolves with the response data for the account.
+ */
 export function getAccount(
     this: RESTBase,
     { accountUuid }: GetAccountRequest
@@ -23,6 +28,12 @@ export function getAccount(
 
 // [GET] List Accounts
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_getaccounts
+/**
+ * List all accounts using the provided request parameters.
+ * 
+ * @param {ListAccountsRequest} requestParams - The parameters for listing accounts.
+ * @returns {Promise<ListAccountsResponse>} A Promise that resolves with the list of accounts.
+ */
 export function listAccounts(
     this: RESTBase,
     requestParams: ListAccountsRequest
