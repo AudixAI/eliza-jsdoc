adac4edf407236124a9fc051e69d2ec71e1c9f53
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2025-01-10 16:08:35 -0500

    docs: Add JSDoc documentation to message.handler.ts

--- packages/plugin-whatsapp/src/handlers/message.handler.ts
+++ packages/plugin-whatsapp/src/handlers/message.handler.ts
@@ -1,9 +1,23 @@
 import { WhatsAppClient } from "../client";
 import { WhatsAppMessage } from "../types";
 
+/**
+ * A class that handles sending WhatsApp messages using a WhatsAppClient instance.
+ */
 export class MessageHandler {
+/**
+ * Constructor for creating a new instance of a class with a specified WhatsAppClient.
+ * @param {WhatsAppClient} client - The WhatsAppClient instance to be used by the class.
+ */
     constructor(private client: WhatsAppClient) {}
 
+/**
+ * Sends a WhatsApp message using the provided message object.
+ * 
+ * @param {WhatsAppMessage} message The WhatsApp message to be sent.
+ * @returns {Promise<any>} A promise that resolves to the response data.
+ * @throws {Error} If there was an error sending the message.
+ */
     async send(message: WhatsAppMessage): Promise<any> {
         try {
             const response = await this.client.sendMessage(message);
