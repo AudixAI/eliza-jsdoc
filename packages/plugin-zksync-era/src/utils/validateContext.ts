5ff49736d43dc04cb27c6435f59c655f74e8e48d
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2025-01-10 16:08:45 -0500

    docs: Add JSDoc documentation to validateContext.ts

--- packages/plugin-zksync-era/src/utils/validateContext.ts
+++ packages/plugin-zksync-era/src/utils/validateContext.ts
@@ -1,7 +1,18 @@
 import { TransferContent } from "../actions/";
 import { isAddress } from "viem";
 
+/**
+ * Validates the transfer action content.
+ * @param {TransferContent} content - The transfer content to be validated.
+ * @returns {boolean} Returns true if the content is valid, false otherwise.
+ */
 export class ValidateContext {
+/**
+ * Checks if the provided content object is a valid TransferContent object.
+ * 
+ * @param {TransferContent} content - The content object to be validated.
+ * @returns {boolean} Returns true if the content object is a valid TransferContent object, otherwise false.
+ */
     static transferAction(
         content: TransferContent
     ): content is TransferContent {
