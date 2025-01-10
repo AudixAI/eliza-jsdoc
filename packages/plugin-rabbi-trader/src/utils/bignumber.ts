d0343cd5f40aa1d56cbbe91540735b20e5686d4c
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2025-01-10 16:07:05 -0500

    docs: Add JSDoc documentation to bignumber.ts

--- packages/plugin-rabbi-trader/src/utils/bignumber.ts
+++ packages/plugin-rabbi-trader/src/utils/bignumber.ts
@@ -4,6 +4,12 @@ import BigNumber from "bignumber.js";
 export const BN = BigNumber;
 
 // Helper function to create new BigNumber instances
+/**
+ * Converts the input value to a BigNumber object.
+ * 
+ * @param {string | number | BigNumber} value - The value to be converted to a BigNumber.
+ * @returns {BigNumber} The converted BigNumber object.
+ */
 export function toBN(value: string | number | BigNumber): BigNumber {
     return new BigNumber(value);
 }
