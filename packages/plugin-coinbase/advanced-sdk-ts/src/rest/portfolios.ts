1a97836b9a3be99a142d05a7e6a85228ce89b9bd
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2025-01-10 16:03:48 -0500

    docs: Add JSDoc documentation to portfolios.ts

--- packages/plugin-coinbase/advanced-sdk-ts/src/rest/portfolios.ts
+++ packages/plugin-coinbase/advanced-sdk-ts/src/rest/portfolios.ts
@@ -18,6 +18,12 @@ import { method } from './types/request-types';
 
 // [GET] List Portfolios
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_getportfolios
+/**
+ * Function to list portfolios.
+ * 
+ * @param {ListPortfoliosRequest} requestParams - The request parameters for listing portfolios.
+ * @returns {Promise<ListPortfoliosResponse>} A promise that resolves with the response containing a list of portfolios.
+ */
 export function listPortfolios(
     this: RESTBase,
     requestParams: ListPortfoliosRequest
@@ -32,6 +38,12 @@ export function listPortfolios(
 
 // [POST] Create Portfolio
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_createportfolio
+/**
+ * Creates a new portfolio based on the provided request parameters.
+ * 
+ * @param {CreatePortfolioRequest} requestParams - The parameters for creating the portfolio.
+ * @returns {Promise<CreatePortfolioResponse>} A promise that resolves with the response from the server.
+ */
 export function createPortfolio(
     this: RESTBase,
     requestParams: CreatePortfolioRequest
@@ -46,6 +58,12 @@ export function createPortfolio(
 
 // [POST] Move Portfolio Funds
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_moveportfoliofunds
+/**
+ * Move funds from one portfolio to another.
+ * 
+ * @param {MovePortfolioFundsRequest} requestParams - The parameters for moving funds.
+ * @returns {Promise<MovePortfolioFundsResponse>} A Promise that resolves with the response data after moving funds.
+ */
 export function movePortfolioFunds(
     this: RESTBase,
     requestParams: MovePortfolioFundsRequest
@@ -60,6 +78,13 @@ export function movePortfolioFunds(
 
 // [GET] Get Portfolio Breakdown
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_getportfoliobreakdown
+/**
+ * Retrieves the breakdown of a portfolio by making a request to the API endpoint.
+ * @param {Object} options - The options for the request.
+ * @param {string} options.portfolioUuid - The UUID of the portfolio to retrieve the breakdown for.
+ * @param {...any} options.requestParams - Additional parameters for the request.
+ * @returns {Promise<GetPortfolioBreakdownResponse>} The response containing the breakdown of the portfolio.
+ */
 export function getPortfolioBreakdown(
     this: RESTBase,
     { portfolioUuid, ...requestParams }: GetPortfolioBreakdownRequest
@@ -74,6 +99,12 @@ export function getPortfolioBreakdown(
 
 // [DELETE] Delete Portfolio
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_deleteportfolio
+/**
+ * Deletes a portfolio with the specified UUID.
+ *
+ * @param {DeletePortfolioRequest} options - The options for deleting the portfolio, including the portfolio UUID.
+ * @returns {Promise<DeletePortfolioResponse>} A promise that resolves with the response from the API after deleting the portfolio.
+ */
 export function deletePortfolio(
     this: RESTBase,
     { portfolioUuid }: DeletePortfolioRequest
@@ -87,6 +118,13 @@ export function deletePortfolio(
 
 // [PUT] Edit Portfolio
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_editportfolio
+/**
+ * Edit a portfolio with the provided UUID by sending a PUT request to the API.
+ * 
+ * @param {EditPortfolioRequest} options - The options for the portfolio edit request.
+ * @param {string} options.portfolioUuid - The UUID of the portfolio to edit.
+ * @returns {Promise<EditPortfolioResponse>} A promise that resolves with the response from the server.
+ */
 export function editPortfolio(
     this: RESTBase,
     { portfolioUuid, ...requestParams }: EditPortfolioRequest
